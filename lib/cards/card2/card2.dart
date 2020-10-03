fbe63add172958ae045e967db01e21cd71fc450a
yaymalaga <yaymalaga@gmail.com>
2020-10-03 18:36:31 +0200

    Unify card2 and card8

--- lib/cards/card2/card2.dart
+++ lib/cards/card2/card2.dart
@@ -1,7 +1,4 @@
-import 'package:cards_ui/components/header.dart';
-import 'package:cards_ui/components/simple_card.dart';
-import 'package:cards_ui/components/user_avatar.dart';
-import 'package:cards_ui/utils/users.dart';
+import 'package:cards_ui/components/users_list_card.dart';
 import 'package:flutter/material.dart';
 
 class Card2 extends StatelessWidget {
@@ -18,59 +15,9 @@ class Card2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleCard(
-      height: 575,
-      child: Column(
-        children: [
-          const Header(
-            titleColor: Color(0xff8f94a2),
-            title: "My messages",
-          ),
-          for (var i = 0; i < _people.length; i++) ...[
-            Header(
-              avatar: UserAvatar(
-                user: usersData[_people[i]],
-              ),
-              title: usersData[_people[i]].fullName,
-              subtitle: usersData[_people[i]].subtitle,
-              subtitleSize: 13,
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "04:00",
-                    style: TextStyle(
-                      color: Color(0xff858997),
-                      fontSize: 12,
-                    ),
-                  ),
-                  if (usersData[_people[i]].notifications != null)
-                    Container(
-                      margin: const EdgeInsets.only(left: 8),
-                      height: 26,
-                      width: 26,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffe3e7fb),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "${usersData[_people[i]].notifications}",
-                          style: const TextStyle(
-                            color: Color(0xff465de2),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            if (i != _people.length - 1) const SizedBox(height: 10),
-          ],
-        ],
-      ),
+    return const UsersListCard(
+      people: _people,
+      trailingDetails: true,
     );
   }
 }
--- lib/cards/card8/card8.dart
+++ lib/cards/card8/card8.dart
@@ -1,7 +1,4 @@
-import 'package:cards_ui/components/header.dart';
-import 'package:cards_ui/components/simple_card.dart';
-import 'package:cards_ui/components/user_avatar.dart';
-import 'package:cards_ui/utils/users.dart';
+import 'package:cards_ui/components/users_list_card.dart';
 import 'package:flutter/material.dart';
 
 class Card8 extends StatelessWidget {
@@ -18,28 +15,9 @@ class Card8 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleCard(
-      height: 575,
-      child: Column(
-        children: [
-          const Header(
-            titleColor: Color(0xff8f94a2),
-            title: "Followers",
-          ),
-          for (var i = 0; i < _people.length; i++) ...[
-            Header(
-              avatar: UserAvatar(
-                user: usersData[_people[i]],
-              ),
-              title: usersData[_people[i]].fullName,
-              subtitle: usersData[_people[i]].fullName,
-              subtitleSize: 13,
-              trailing: const SizedBox(),
-            ),
-            if (i != _people.length - 1) const SizedBox(height: 10),
-          ],
-        ],
-      ),
+    return const UsersListCard(
+      people: _people,
+      trailingDetails: true,
     );
   }
 }
